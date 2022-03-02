484895fec230a9baa7c0697a35fa9f1a13953b05
chloefont <cfontaine@citycable.ch>
2022-03-02 13:46:36 +0100

    mouvement + couleur ok

--- src/graphics/MainWindow.java
+++ src/graphics/MainWindow.java
@@ -2,11 +2,15 @@ package graphics;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.ImageObserver;
 
 public class MainWindow implements Displayer {
     private static MainWindow instance;
     private Canvas canvas;
     private JFrame frame;
+    private Image image;
+    private Graphics2D g;
+
     private MainWindow() {
         canvas = new Canvas();
         frame = new JFrame();
@@ -17,6 +21,9 @@ public class MainWindow implements Displayer {
         frame.setVisible(true);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        image = canvas.createImage(getWidth(), getHeight());
+        g = (Graphics2D) image.getGraphics();
     }
 
     public static MainWindow getInstance() {
@@ -29,23 +36,27 @@ public class MainWindow implements Displayer {
 
     @Override
     public int getWidth() {
-        return frame.getWidth();
+        return canvas.getWidth();
     }
 
     @Override
     public int getHeight() {
-        return frame.getHeight();
+        return canvas.getHeight();
     }
 
     @Override
     public Graphics2D getGraphics() {
-        return (Graphics2D) frame.getGraphics();
+        return g;
     }
 
     @Override
     public void repaint() {
-        frame.repaint();
+        //frame.repaint();
         //canvas.repaint();
+
+        canvas.getGraphics().drawImage(image, 0, 0, null);
+        g.setColor(frame.getBackground());
+        g.fillRect(0, 0, getWidth(), getHeight());
     }
 
     @Override
