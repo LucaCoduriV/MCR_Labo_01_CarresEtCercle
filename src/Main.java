95daaf3e669818e1d92d66c3c49382c4bbbd9c03
Luca Coduri <coduri.vitor@gmail.com>
2022-03-03 10:33:18 +0100

    cleaning

--- src/Main.java
+++ src/Main.java
@@ -19,58 +19,41 @@ import java.util.Random;
 
 public class Main {
 
-    private static Color CIRCLE_COLOR;
-    private static Color SQUARE_COLOR;
-    private static int NB_CIRCLES;
-    private static int NB_SQUARES;
-    // TODO laisser ici ?
-    private static int MAX_SIZE;
-    private static int MIN_SIZE;
+    private static final Color CIRCLE_COLOR = Color.BLUE;
+    private static final Color SQUARE_COLOR = Color.ORANGE;
+    private static final int NB_CIRCLES = 15;
+    private static final int NB_SQUARES = 15;
 
-    private static int MAX_MOVEMENT;
-    private static int MIN_MOVEMENT;
+    private static final int MAX_SIZE = 30;
+    private static final int MIN_SIZE = 10;
+
+    private static final int MAX_MOVEMENT = 4;
+    private static final int MIN_MOVEMENT = 1;
+    private static final int REFRESH_RATE = 1000/60;
 
     public static void main(String[] args) {
-        CIRCLE_COLOR = Color.BLUE;
-        SQUARE_COLOR = Color.ORANGE;
-        NB_CIRCLES = 15;
-        NB_SQUARES = 15;
-
-        MAX_SIZE = 30;
-        MIN_SIZE = 10;
-
-        MAX_MOVEMENT = 4;
-        MIN_MOVEMENT = 1;
-
 
         List<DShape> shapes = new ArrayList<>();
         MainWindow mainWindow = MainWindow.getInstance();
-
 
         try{
             createShapeRandomly(DCircle.class,CIRCLE_COLOR, NB_CIRCLES, mainWindow, shapes);
             createShapeRandomly(DSquare.class,SQUARE_COLOR, NB_SQUARES, mainWindow, shapes);
         }catch(Exception e){
-            System.out.println(e);
+            System.err.println(e);
+            return;
         }
 
+        Timer timer = new Timer(REFRESH_RATE, e -> {
+            mainWindow.repaint();
 
+            for (DrawableObject d : shapes)
+                d.update();
 
-
-
-        Timer timer = new Timer(1000 / 60, new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                mainWindow.repaint();
-
-                for (DrawableObject d : shapes)
-                    d.update();
-
-                for (Drawable d : shapes) {
-                    d.draw();
-                }
-
+            for (Drawable d : shapes) {
+                d.draw();
             }
+
         });
 
         timer.start();
@@ -87,7 +70,7 @@ public class Main {
             final int MOVE_X = random.nextInt((MAX_MOVEMENT - MIN_MOVEMENT) * 2) - MIN_MOVEMENT;
             final int MOVE_Y = random.nextInt((MAX_MOVEMENT - MIN_MOVEMENT) * 2) - MIN_MOVEMENT;
 
-            Constructor constr = shape.getDeclaredConstructor(int.class,
+            Constructor<?> constr = shape.getDeclaredConstructor(int.class,
                     Color.class,
                     Vector.class,
                     Vector.class,
